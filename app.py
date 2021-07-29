39b957c9ac8697eb00cf232eb43e5db7cae99b6f
antonio00 <marcos_leite00@hotmail.com>
2021-07-29 17:52:54 -0300

    validacao

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, redirect, request
+import ctypes
 
 app = Flask(__name__)
 
@@ -8,9 +9,14 @@ def index():
 
 @app.route('/home', methods=['GET', 'POST'])
 def home():
-    if request.method == 'POST':
+    if request.form['usuario'] =='admin' and request.form['senha']=='admin':
              
         return render_template('home.html')
+    else:
+        ctypes.windll.user32.MessageBoxW(0, "Usuário e Senha não conferem"),
+        return render_template('index.html')
+
+        
         
 
 if __name__ == '__main__':
